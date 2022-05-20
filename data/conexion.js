4ebbc72a48fc76bf1a506faff27479af57c6f502
Vero <101568289+verovede@users.noreply.github.com>
2022-05-20 01:20:34 -0300

    Update conexion.js

--- data/conexion.js
+++ data/conexion.js
@@ -6,7 +6,6 @@ const mongoclient = require('mongodb').MongoClient
 
 
 // la uri esta en mongodb en connect, connect to aplication
-//const uri = "mongodb+srv://admin:tallerort22@cluster0.yocxw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const uri = process.env.MONGO_URI;
 
 const client = new mongoclient(uri);
